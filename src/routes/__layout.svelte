0b06460c7177351f1e0dd574386bebd20b7c8eb8
zhuganglie <pyrrhonianpig@gmail.com>
2021-06-23 09:19:03 +0800

    update

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -4,11 +4,14 @@
 	import '../app.css';
 </script>
 
+<section class="flex-col justify-between items-center h-screen">
 <Header />
 
-<main class="p-2 md:p-8 w-full max-w-screen-lg  mx-auto box-border h-auto overflow-y-auto">
+<main class="p-2 md:p-8 w-full max-w-screen-lg  mb-auto box-border h-auto overflow-y-auto">
 	<slot />
 </main>
 
 <Footer />
+</section>
+
 
