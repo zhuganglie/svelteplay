ea6ab63985c09ef60f7cde0d94d696311821a957
zhuganglie <pyrrhonianpig@gmail.com>
2021-06-23 09:25:22 +0800

    update

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -7,7 +7,7 @@
 <section class="flex-col justify-between items-center h-screen">
 <Header />
 
-<main class="p-2 md:p-8 w-full max-w-screen-lg  mb-auto box-border h-10 overflow-y-auto">
+<main class="p-2 md:p-8 w-full max-w-screen-lg  mb-auto box-border h-auto overflow-y-auto">
 	<slot />
 </main>
 
