740a67b91668957229b7b77385ab2c8670572e54
zhuganglie <iwifer@gmail.com>
2021-06-25 02:07:29 +0800

    add disqus

--- src/lib/postLayout.svelte
+++ src/lib/postLayout.svelte
@@ -1,11 +1,15 @@
+
 <script>
+import { page } from '$app/stores';
+
+	import Comments from 'disqus-svelte';
 	import {formatDate} from './date';
-    
+	
 	export let title;
 	export let date;
 
     let dateDisplay = formatDate(date);
-	
+
 </script>
 
 <svelte:head>
@@ -17,8 +21,8 @@
 		<div class="font-bold text-4xl mb-4">{title}</div>
 		<div class="text-sm">{dateDisplay}</div>
 	</div>
-	
 	<slot />
 	<hr />
 	<a href="/blog/" class="bg-yellow-100 rounded-full px-2">&larr; Back to List</a>
+	<Comments identifier="post" />
 </div>
