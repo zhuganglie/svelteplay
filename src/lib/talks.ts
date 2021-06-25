affaf8b2f15f3333516d63080c747c796a19bb6a
zhuganglie <iwifer@gmail.com>
2021-06-26 02:02:54 +0800

    add talk

--- src/lib/talks.ts
+++ src/lib/talks.ts
@@ -3,15 +3,15 @@ export const talks = [
       "title": "How to Prioritize Tasks",
       "date": "2021-06-20",
       "description":
-        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum repudiandae, autem eos voluptatum assumenda cum laborum iure nam in accusantium.",
+        "This talk introduce the ways to prioritize tasks and manage time effectively.",
       "slide": "/pdfs/prioritize.pdf",
       "image": "/images/talk.png"
     },
     {
-      "title": "Positive Way of Thinking",
+      "title": "How to Develop Can-Do Attitude",
       "date": "2018-09-10",
       "description":
-        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum repudiandae, autem eos voluptatum assumenda cum laborum iure nam in accusantium.",
+        "We can be a person with can-do attitude by developing a positive way of thinking.",
       "slide": "/pdfs/facilitation.pdf",
       "image": "/images/talk.png"
     } 
--- src/routes/talks/index.svelte
+++ src/routes/talks/index.svelte
@@ -8,9 +8,9 @@
     
         {#each talks as talk}
         {#if formatDate(talk.date)}
-        <div class="md:px-10 md:flex md:space-x-4 md:space-y-4 md:justify-center md: items-center">
+        <div class=" md:flex md:space-x-4 md:space-y-4 md:justify-center md: items-center">
             <img src={talk.image} alt="talk1" loading="lazy" class="w-full md:w-1/4 block ml-auto mr-auto mb-4" />
-            <div>
+            <div class="w-full md:w-3/4">
             <h4>{talk.title}</h4>
             <p>{talk.description}</p>
             <a href={talk.slide} target="_blank" class="flex items-center w-max px-2 ">下载 PDF</a>
