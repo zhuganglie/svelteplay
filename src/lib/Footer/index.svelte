75e07be8187ff62547ff89ca9f13606f0b01a0ac
zhuganglie <iwifer@gmail.com>
2021-07-07 16:31:20 +0800

    add email to footer

--- src/lib/Footer/index.svelte
+++ src/lib/Footer/index.svelte
@@ -3,5 +3,5 @@
 </script>
 
 <footer class="sticky w-full py-2 h-10 bottom-0 text-sm text-center bg-yellow-50 shadow-inner flex-shrink-0">
-    <p>Copyright &copy; {new Date().getFullYear()} Caesar </p> 
+    <p>Copyright &copy; {new Date().getFullYear()} <a href="mailto:pyrrhonianpig@gmail.com">Caesar</a></p> 
 </footer>
