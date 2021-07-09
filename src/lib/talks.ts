ece6f73340579f597405ac400e625e2df57ca141
zhuganglie <iwifer@gmail.com>
2021-07-10 01:07:33 +0800

    add talks

--- src/lib/talks.ts
+++ src/lib/talks.ts
@@ -3,16 +3,23 @@ export const talks = [
       "title": "How to Prioritize Tasks",
       "date": "2021-06-20",
       "description":
-        "This presentation decribes how we can manage time effectively by prioritizing tasks.",
+        "This presentation explains how we can manage time effectively by prioritizing tasks.",
       "slide": "/pdfs/prioritize.pdf",
       "image": "/images/talk.png"
     },
     {
-      "title": "How to Develop Can-Do Attitude",
+      "title": "Developing Can-Do Attitude",
       "date": "2018-09-10",
       "description":
-        "We can develop a can-do attitude by learning positive way of thinking.",
+        "This talk introduces a way to develop can-do attitude.",
       "slide": "/pdfs/facilitation.pdf",
       "image": "/images/talk.png"
-    } 
+    } ,
+    {
+      "title": "Decoding Body Language",
+      "date": "2021-07-09",
+      "description": "This talk explains what body language is and how to read body language.",
+      "slide": "/pdfs/body.pdf",
+      "image": "/images/talk.png"
+    }
 ]
