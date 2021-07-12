fbee85d07d5e9f176fd0efa543ce4a8ee0eeb12f
zhuganglie <iwifer@gmail.com>
2021-07-12 16:40:43 +0800

    footer css

--- src/lib/Footer/index.svelte
+++ src/lib/Footer/index.svelte
@@ -2,6 +2,6 @@
 	import { page } from '$app/stores';
 </script>
 
-<footer class="sticky w-full py-2 h-10 bottom-0 text-sm text-center bg-yellow-50 shadow-inner flex-shrink-0">
-    <p>Copyright &copy; {new Date().getFullYear()} <a href="mailto:pyrrhonianpig@gmail.com">Caesar</a></p> 
+<footer class="sticky w-full py-3 h-10 bottom-0 text-sm text-center bg-yellow-50 shadow-inner flex-shrink-0">
+    <p>Copyright &copy; {new Date().getFullYear()} <a href="mailto:pyrrhonianpig@gmail.com" class="bg-yellow-100 rounded px-2">Caesar</a></p> 
 </footer>
