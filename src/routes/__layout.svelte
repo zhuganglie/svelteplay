cd6d1e12717991784e7fe584d034cd77ce1dce35
zhuganglie <pyrrhonianpig@gmail.com>
2021-09-06 16:39:15 +0800

    update blog css

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 	import Header from '$lib/Header/index.svelte';
 	import Footer from '$lib/Footer/index.svelte';
-	import '../app.css';
+	import '../app.postcss';
 </script>
 
 <div class="flex flex-col min-h-screen">
--- src/routes/blog/index.svelte
+++ src/routes/blog/index.svelte
@@ -23,6 +23,7 @@
 <script lang="ts">
     import {formatDate} from '../../lib/date';
     export let posts;
+    export let tags;
     const dateSortedPosts = posts.slice().sort((a, b) => {
         return Date.parse(b.metadata.date) - Date.parse(a.metadata.date);
     });  
@@ -35,10 +36,18 @@
 <h2>博 客</h2>
 <hr />
 
-{#each dateSortedPosts as {path, metadata: {title, date}}}
-<div class="flex justify-between items-center">
-    <a href={`/blog/${path.replace(".md", "").replace(".svx", "")}`} class="block mb-4 pb-4">{title}</a>
-    <p class="block mb-4 pb-4">
+{#each dateSortedPosts as {path, metadata: {title, date, tags}}}
+<div class="flex justify-between">
+    <div class="mb-4 pb-4 ">
+    <a href={`/blog/${path.replace(".md", "").replace(".svx", "")}`} class="text-md text-left mb-2 text-gray-900">{title}</a>
+    <div class="flex justify-start">
+        {#each tags as tag}
+          <a sveltekit:prefetch class="rounded bg-gray-100 px-2 py-0.5 mx-1.5 text-sm " href="/tags/{tag}"
+            >{tag}</a>
+        {/each}
+        </div>
+      </div>
+    <p class="block mb-4 pb-4 text-base">
         {formatDate(date)}
     </p>
 </div>
