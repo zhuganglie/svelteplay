28f678115d8487a5e7c56e60de6dab301f34fb78
zhuganglie <iwifer@gmail.com>
2021-09-19 01:30:39 +0800

    update blog css

--- src/lib/Footer/index.svelte
+++ src/lib/Footer/index.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
-	import { page } from '$app/stores';
+	// import { page } from '$app/stores';
 </script>
 
 <footer class="sticky w-full py-3 h-10 bottom-0 text-sm text-center bg-yellow-50 shadow-inner flex-shrink-0">
-    <p>Copyright &copy; {new Date().getFullYear()} <a href="mailto:pyrrhonianpig@gmail.com" class="bg-gray-100 rounded px-2">zhuganglie</a></p> 
+<p>Copyright &copy; {new Date().getFullYear()} <a href="mailto:pyrrhonianpig@gmail.com" class="rounded text-gray-900 px-2">zhuganglie</a></p> 
 </footer>
