f0f848c3099bfca74a68762edc8c64bae24b216b
zhuganglie <iwifer@gmail.com>
2021-09-23 22:36:04 +0800

    change footer

--- src/lib/footer/index.svelte
+++ src/lib/footer/index.svelte
@@ -3,5 +3,5 @@
 </script>
 
 <footer class="sticky w-full py-3 h-10 bottom-0 text-sm text-center bg-yellow-50 shadow-inner flex-shrink-0">
-<p>Copyright &copy; {new Date().getFullYear()} <a href="mailto:pyrrhonianpig@gmail.com" class="rounded text-gray-900 px-2">zhuganglie</a></p> 
+<p>&copy; 2020 - {new Date().getFullYear()} @<a href="mailto:pyrrhonianpig@gmail.com" class="rounded text-gray-900">一指禅</a></p> 
 </footer>
