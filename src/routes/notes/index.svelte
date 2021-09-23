0c775e5376e0edc02534dd25f5e226aa113f22e0
zhuganglie <iwifer@gmail.com>
2021-09-23 22:29:15 +0800

    update notes css

--- src/routes/notes/index.svelte
+++ src/routes/notes/index.svelte
@@ -15,14 +15,15 @@
     
         {#each notes as note}
         {#if formatDate(note.date)}
-        <div class=" md:flex md:space-x-4 md:space-y-4 md:justify-center md: items-center">
-            <img src={note.image} alt="talk1" loading="lazy" class="w-full md:w-1/4 block ml-auto mr-auto mb-4" />
-            <div class="w-full md:w-3/4">
+        <div class="md:flex md:items-center md:space-y-6 mb-6">
+            <div class=""><img src={note.image} alt="talk1" loading="lazy" class="w-1/2 md:w-1/4 my-4 mx-auto" /></div>
+            <div class="w-full md:w-11/12 m-0">
             <h4>{note.title}</h4>
             <p>{note.description}</p>
             <a href={note.slide} target="_blank" class="rounded w-max px-2.5 py-1 bg-green-900 text-gray-50 hover:text-gray-100 focus:text-gray-100 text-sm flex items-center space-x-2"><span>下载</span><FilePdfOutline {size} />
             </a>
             </div>
         </div>
+        <hr />
         {/if}
         {/each}
