9e9f7d524e62c9f0f48e362ba5455d7b2d6ae7cc
zhuganglie <iwifer@gmail.com>
2021-10-05 00:24:55 +0800

    update

--- src/routes/recipe/index.svelte
+++ src/routes/recipe/index.svelte
@@ -1,7 +1,14 @@
 <script>
-  import dish from "$lib/data/dish.json";
-  let meat;
-  let veg;
+  import dishes from "$lib/data/dish.json";
+  export let meatList = dishes.filter(dish => dish.type === '荤菜');
+  export let vegList = dishes.filter(dish => dish.type === '素菜');
+  
+
+  const veg = vegList.sort(() => Math.random() - Math.random()).slice(0, 2);
+  const meat = meatList.sort(() => Math.random() - Math.random()).slice(0, 2);
+  const menu = meat.concat(veg)
+  function handleClick(){ }
+ 
 </script>
 
 <svelte:head>
@@ -12,7 +19,13 @@
   <hr />
   
 
-
-  {#each dish as item}
-   <li>{item.name}</li>
+<h3>随机菜单</h3>
+<button on:click={handleClick} class="border-2 px-1.5 py-0.5 mb-2 text-green-900 border-green-900 bg-green-50 rounded ">猛击这里</button>
+<ul>
+  {#each menu as item}
+   <li>{item.name} - {item.taste}</li>
   {/each}
+</ul>
+<h3>自选菜单</h3>
+
+<h3>外卖菜单</h3>
