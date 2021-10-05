b1396e795ae851aed6a80659d5c361ae6d080123
zhuganglie <iwifer@gmail.com>
2021-10-06 00:11:21 +0800

    update

--- src/routes/recipe/index.svelte
+++ src/routes/recipe/index.svelte
@@ -1,14 +1,11 @@
 <script>
+  import { onMount } from 'svelte';
   import dishes from "$lib/data/dish.json";
-  export let meatList = dishes.filter(dish => dish.type === '荤菜');
-  export let vegList = dishes.filter(dish => dish.type === '素菜');
-  
-
-  const veg = vegList.sort(() => Math.random() - Math.random()).slice(0, 2);
-  const meat = meatList.sort(() => Math.random() - Math.random()).slice(0, 2);
-  const menu = meat.concat(veg)
-  function handleClick(){ }
- 
+  let meatList = dishes.filter(dish => dish.type === '荤菜');
+  let vegList = dishes.filter(dish => dish.type === '素菜');
+  let veg = vegList.sort(() => Math.random() - Math.random()).slice(0, 2);
+  let meat = meatList.sort(() => Math.random() - Math.random()).slice(0, 2);
+  let menu = meat.concat(veg);
 </script>
 
 <svelte:head>
@@ -20,7 +17,7 @@
   
 
 <h3>随机菜单</h3>
-<button on:click={handleClick} class="border-2 px-1.5 py-0.5 mb-2 text-green-900 border-green-900 bg-green-50 rounded ">猛击这里</button>
+<button on:click={rnd} class=" px-1.5 py-0.5 mb-2 text-green-50 bg-green-900 rounded ">更 新</button>
 <ul>
   {#each menu as item}
    <li>{item.name} - {item.taste}</li>
