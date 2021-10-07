ec08b68d3e946d087d00a043500e2015667efb9f
zhuganglie <iwifer@gmail.com>
2021-10-07 20:39:22 +0800

    update

--- src/routes/recipe/index.svelte
+++ src/routes/recipe/index.svelte
@@ -4,23 +4,22 @@
 
   let meatList = dishes.filter(dish => dish.type === '荤菜');
   let vegList = dishes.filter(dish => dish.type === '素菜');
-  let veg
-  let meat
-  let menu = [];
+  let veg = vegList.sort(() => Math.random() - Math.random()).slice(0, 2);
+  let meat = meatList.sort(() => Math.random() - Math.random()).slice(0, 2);
+  let menu = meat.concat(veg);
 
-  function dish() {
+  export function change() {
     veg = vegList.sort(() => Math.random() - Math.random()).slice(0, 2);
     meat = meatList.sort(() => Math.random() - Math.random()).slice(0, 2);
-    menu = meat.concat(veg)
+    menu = meat.concat(veg);
   }
-  
+
   const rts = [
     {href: 'https://www.startaster.com.cn/phone.php/restaurant/RE000564/zh-cn?', title: '蛙功夫'},
     {href: 'https://www.startaster.com.cn/phone.php/restaurant/RE000524/zh-cn?', title: '湘菜馆'},
     {href: 'https://www.startaster.com.cn/phone.php/restaurant/RE000042/zh-cn?', title: '老成都'},
     {href: 'https://www.cqgf.com.sg/cn/', title: '渝烤鱼'}
   ]
-
   const yts = [
     {href: 'https://www.youtube.com/channel/UCg0m_Ah8P_MQbnn77-vYnYw', title: '厨师长'},
     {href: 'https://www.youtube.com/channel/UCBJmYv3Vf_tKcQr5_qmayXg', title: '老饭骨'},
@@ -38,14 +37,14 @@
   <hr />
   
 <h3>随机菜单</h3>
-<button on:click={dish} class=" px-1.5 py-0.5 mb-2 text-green-50 bg-green-900 rounded ">点我更新</button>
+<button on:click={change} class=" px-1.5 py-0.5 mb-2 text-green-50 bg-green-900 rounded ">点我更新</button>
 <ul>
   {#each menu as item}
    <li>{item.name} - {item.taste}</li>
   {/each}
 </ul>
 <h3>自选菜单</h3>
-<button on:click={vis} class=" px-1.5 py-0.5 mb-8 text-green-50 bg-green-900 rounded ">点我选择</button>
+<button class=" px-1.5 py-0.5 mb-8 text-green-50 bg-green-900 rounded ">点我选择</button>
 
 <h3>他山之食</h3>
 <p>
