ceba2e9c7eca6fac52f0252883956cdf4f0b6b83
zhuganglie <iwifer@gmail.com>
2021-10-22 00:11:49 +0800

    update

--- src/routes/talks/index.svelte
+++ src/routes/talks/index.svelte
@@ -1,26 +1,29 @@
 <script>
     import {formatDate} from "../../lib/date";
-    import notes from "../../lib/data/notes.json";
+    import talks from "$lib/data/talks.json";
     import FilePdfOutline from "svelte-material-icons/FilePdfOutline.svelte";
 
     export let size = "1.15em";
     </script>
 
     <svelte:head>
-        <title>Notes</title>
+        <title>Talks</title>
     </svelte:head>
 
         <h2>报 告</h2>
         <hr />
     
-        {#each notes as note}
-        {#if formatDate(note.date)}
+        {#each talks as talk}
+        {#if formatDate(talk.date)}
         <div class="md:flex md:items-center md:space-y-6 mb-6">
-            <div class=""><img src={note.image} alt="talk1" loading="lazy" class="w-1/2 md:w-1/4 my-4 mx-auto" /></div>
+            <div class="">
+                <img src={talk.image} alt="talk1" loading="lazy" class="w-1/2 md:w-1/4 my-4 mx-auto" />
+            </div>
             <div class="w-full md:w-11/12 m-0">
-            <h4>{note.title}</h4>
-            <p>{note.description}</p>
-            <a href={note.slide} target="_blank" class="rounded w-max px-2.5 py-1 bg-green-900 text-gray-50 hover:text-gray-100 focus:text-gray-100 text-sm flex items-center space-x-2"><span>下载</span><FilePdfOutline {size} />
+            <h4>{talk.title}</h4>
+            <p>{talk.description}</p>
+            <a href={talk.slide} target="_blank" class="rounded w-max px-2.5 py-1 bg-green-900 text-gray-50 hover:text-gray-100 focus:text-gray-100 text-sm flex items-center space-x-2">
+                <span>详 情</span><FilePdfOutline {size} />
             </a>
             </div>
         </div>
