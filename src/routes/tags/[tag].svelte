fb93497c73e16191ebc019e7c6d16812f9c5c27c
zhuganglie <iwifer@gmail.com>
2021-10-25 23:42:44 +0800

    update

--- src/routes/tags/[tag].svelte
+++ src/routes/tags/[tag].svelte
@@ -43,4 +43,4 @@
 </li>
 {/each}
 <hr />
-<a href="/blog/" class="bg-green-900 text-gray-50 hover:text-gray-100 focus:text-gray-100 rounded px-2.5 py-0.5">&larr; 返回列表</a>
+<a href="/blog/" class="bg-gray-700 text-gray-300 hover:text-gray-100 focus:text-gray-100 rounded px-2.5 py-0.5">&larr; 返回列表</a>
