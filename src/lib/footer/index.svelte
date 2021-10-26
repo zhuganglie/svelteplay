d63541c6c1163da67b56aa3f511cc3f1c9e8b801
zhuganglie <iwifer@gmail.com>
2021-10-27 00:05:45 +0800

    update

--- src/lib/footer/index.svelte
+++
@@ -1,7 +0,0 @@
-<script lang="ts">
-	// import { page } from '$app/stores';
-</script>
-
-<footer class="sticky w-full py-3 h-10 bottom-0 text-sm text-center bg-yellow-50 shadow-inner flex-shrink-0">
-<p>&copy; 2020 - {new Date().getFullYear()} @<a href="mailto:pyrrhonianpig@gmail.com" class="rounded text-gray-900">一指禅</a></p> 
-</footer>
