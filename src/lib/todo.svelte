ab03fc341acb1f48aa5125dc41be8d07e66138d0
zhuganglie <iwifer@gmail.com>
2021-11-08 19:23:49 +0800

    update

--- src/lib/todo.svelte
+++ src/lib/todo.svelte
@@ -1,44 +1,68 @@
 <script>
     import Close from 'svelte-material-icons/Close.svelte'
+    import supabase from '$lib/db';
 
-    let size;
-    let newItem = '';
+async function getData() {
+const { data, error } = await supabase
+.from('todos')
+.select('*')
+if (error) throw new Error(error.message)
 
-    let todoList = [
-        {text:"运动三十分钟", status:false},
-        {text:"吃三餐饭", status:false},
-        {text:"挣三百块钱", status:true},
-        {text:"睡觉休息", status:false},
-    ];
-	
-	function addToList() {
-		todoList = [...todoList, {text: newItem, status: false}];
-		newItem = '';
-	}
-	
-	function removeFromList(index) {
-		todoList.splice(index, 1)
-		todoList = todoList;
-    }
+ return data
+}    
+
+let size;
+let newItem = ''
+let submit = false
+
+async function sendData() {
+  const { data, error } = await supabase
+    .from('todos')
+    .insert([
+      { 'text': newItem }
+    ])
+  if (error) throw new Error(error.message) 
+  return data
+}
+
+async function deleteData(){
+const { data, error } = await supabase
+  .from('todos')
+  .delete()
+  .eq('id', '')
+
+  if (error) throw new Error(error.message) 
+  return data
+}
 </script>
 
-<div class="flex flex-col space-y-1">
-<input bind:value={newItem} type="text" placeholder="今天要做的事项" class="bg-gray-700 text-gray-300">
-<button on:click={addToList} class="py-1 px-2 bg-gray-700 text-gray-300">添加</button>
-</div>
-<br/>
-{#each todoList as item, index}
+
+{#await getData()}
+  <p>Fetching data...</p>
+{:then data}
+{#each data as todo}
 <div class="flex justify-between items-center">
-	<input bind:checked={item.status} type="checkbox">
-	<span class:checked={item.status}>{item.text}</span>
-	<span on:click={() => removeFromList(index)}><Close {size} /></span>
+	<span>{todo.text}</span>
+	
     </div>
 	<br/>
 {/each} 
+{:catch error}
+  <p>Something went wrong while fetching the data:</p>
+  <pre>{error}</pre>
+{/await}
 
-
-<style> 
-	.checked {
-        text-decoration: line-through;
-    }
-</style> 
+<form on:submit|preventDefault={() => submit = true} class="flex flex-col space-y-1">
+    <input type="text" bind:value={newItem} placeholder="今天要做的事项" class="bg-gray-700 text-gray-300">
+    <input type="submit" value="提交" on:click={() => submit = false} class="py-1 px-2 bg-gray-700 text-gray-300">
+  </form>
+  {#if submit}
+    {#await sendData()}
+      <p>Sending data...</p>
+    {:then data}
+      <p>Succesfully sent data.</p>
+    {:catch error}
+      <p>Something went wrong while sending the data:</p>
+      <pre>{error}</pre>
+    {/await}
+  {/if}
--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -22,8 +22,7 @@
 </script>
 
 <div class="flex h-screen">
-    {#if open}
-    <aside class="flex md:flex flex-col items-center justify-evenly w-full md:w-1/5 border-r-4 border-gray-700 px-2 min-w-max" class:hidden={!open} in:fly="{{ x: -100, duration: 500 }}" out:fly="{{ x: -100, duration: 500 }}">
+    <aside class="flex md:flex flex-col items-center justify-evenly w-full md:w-1/5 border-r-4 border-gray-700 px-2 min-w-max" class:hidden={!open}>
    <header class="text-center">
        <p class="text-2xl font-bold">一指禅</p>
    <p class="text-sm text-yellow-500">惯看秋月春风</p>
@@ -41,7 +40,6 @@
    <span>&copy 2020 - {new Date().getFullYear()}</span>
 </footer>
 </aside>
-{/if}
 
     <main class="pt-8 px-4 md:px-6 mx-auto w-full md:w-3/5 overflow-auto">
         <ClickOutside on:clickoutside="{() => (open = false)}">
