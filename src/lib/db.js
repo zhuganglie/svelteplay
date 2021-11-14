c209ca33be0e853bb21e83ce765dbc2ddfb6a8b6
zhuganglie <iwifer@gmail.com>
2021-11-14 23:39:30 +0800

    remove db

--- src/lib/db.js
+++
@@ -1,8 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-const supabase = createClient(
-   // @ts-ignore
-   import.meta.env.VITE_SUPABASE_URL,
-   // @ts-ignore
-   import.meta.env.VITE_SUPABASE_ANON_KEY
-)
-export default supabase
