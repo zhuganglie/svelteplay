83092911dbff9f369e2dac12ab8b9dfefdf8564a
zhuganglie <pyrrhonianpig@gmail.com>
2021-12-18 16:15:44 +0800

    update

--- mdsvex.config.js
+++ mdsvex.config.js
@@ -1,21 +1,21 @@
 //import { createRequire } from "module";
 //const require = createRequire(import.meta.url)
 //export const mdsvexConfig = require("./mdsvex.config.cjs");
-import github from 'remark-github';
+import * as github from "remark-github";
 
 const config = {
   "extensions": [".svelte.md", ".md", ".svx"],
 
-  layout: {
-		blog: './src/lib/templates/post.svelte',
-		slides: './src/lib/templates/slide.svelte',
+  "layout": {
+		"blog": "./src/lib/templates/post.svelte",
+		"slides": "./src/lib/templates/slide.svelte",
 	},
 
   "smartypants": {
     "dashes": "oldschool"
   },
 
-  "remarkPlugins": [github],
+  "remarkPlugins": ["github"],
   "rehypePlugins": [],
 };
 
