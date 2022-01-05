79a2fa9d8ee0e67ad9fa11be4e5f7f7d7139260f
zhuganglie <iwifer@gmail.com>
2022-01-05 19:13:14 +0800

    update

--- src/routes/words/index.svelte
+++ src/routes/words/index.svelte
@@ -1,19 +1,22 @@
 <script lang="ts">
 import Words from '$lib/data/words.json'
 import { Splide, SplideSlide } from '@splidejs/svelte-splide';
-  import type { Options } from '@splidejs/splide';
-  import '@splidejs/splide/dist/css/splide.min.css';
+import type { Options } from '@splidejs/splide';
+import '@splidejs/splide/dist/css/splide.min.css';
+
   const options: Options = {
     rewind : true,
     autoplay: true,
     perPage: 1,
+    //direction: 'ttb',
     gap    : '1rem',
     height : '30rem',
     cover: false,
     mediaQuery: 'max',
     breakpoints: {
-      1000: {
+      1024: {
         height: '35rem',
+        perpage: 1,
       },
     },
   }
@@ -25,12 +28,12 @@ import { Splide, SplideSlide } from '@splidejs/svelte-splide';
   <Splide options={options}>
     {#each Words as word}
  <SplideSlide>
-   <div class="h-full w-4/5 grid place-content-center m-auto">
+   <div class="h-full w-5/6 grid place-content-center m-auto">
     <details>
-      <summary class="text-xl mb-6">{word.word}</summary>
-      <div class="flex">
+      <summary class="text-3xl mb-6">{word.word}</summary>
+      <div class="flex space-x-8">
         {#each word.content as item}
-        <div>
+        <div class="">
         <p>读音：{item.pronunciation}</p>
         <p>词性：{item.property}</p>
         <p>解释：{item.meaning}</p>
@@ -38,7 +41,7 @@ import { Splide, SplideSlide } from '@splidejs/svelte-splide';
         </div>
         {/each}
         </div>
-        <p>近义词：</p>
+        <p>近义词：{#each word.synonym as item}<span class="mr-2">{item}</span>{/each}</p>
         <p>看图：</p>
           <p>视频：</p>
           <p>音频：</p>
