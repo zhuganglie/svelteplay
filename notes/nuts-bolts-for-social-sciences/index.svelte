a57a87d30fab4711b6bf0c2ab2eebaa7c54af123
zhuganglie <iwifer@gmail.com>
2022-02-18 09:38:23 +0800

    add note

---
+++ notes/nuts-bolts-for-social-sciences/index.svelte
@@ -0,0 +1,12 @@
+---
+title: 社会科学的螺帽和螺母
+date: 2022-02-15
+book: "Nuts and Bolts for the Social Sciences"
+authors: ["Jon Elster"]
+publisher: Cambridge University Press
+year: 1989
+categories: ["社会科学", "方法论"]
+draft: false
+---
+
+This book is intended as an introductory survey of the philosophy of the social sciences. It is essentially a work of exposition that offers a tool box of mechanisms--nuts and bolts, cogs and wheels--that can be used to explain complex social phenomena. Within a brief compass, Jon Elster covers a vast range of topics. His point of departure is the conflict we all face between our desires and our opportunities. How can rational choice theory help us understand our motivation and behavior? More significantly, what happens when the theory breaks down but we still cleave to a belief in the power of the rational? Elster describes the fascinating range of forms of irrationality--wishful thinking, the phenomenon of sour grapes, discounting the future in non-cooperative behavior. He shows how these issues bear directly on our lives in such concrete situations as wage bargaining, economic cartels, political strikes, voting in elections, and court decisions involving child custody.
